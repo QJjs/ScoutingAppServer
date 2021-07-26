b9e73ec8a9c4470a7faf8f4d12254f4400f7b5a7
QJ.js <peder@qjjs.us>
2021-07-26 14:03:35 -0500

    I'm a teapot

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,8 @@ const mimeTypes = {
 	'.png': 'image/png',
 	'.jpg': 'image/jpg',
 	'.gif': 'image/gif',
+	'.xml': 'text/xml',
+	'.txt': 'text/plain',
 	'.svg': 'image/svg+xml',
 	'.wav': 'audio/wav',
 	'.mp4': 'video/mp4',
@@ -26,13 +28,30 @@ const mimeTypes = {
 };
 
 http.createServer(options, (request: http.IncomingMessage, response: http.ServerResponse) => {
+
+	request.url = request.url.replace(/%20/g, " ")
+	if(request.method == "DELETE") {
+		if (!request.url.startsWith("/forms")) {
+			response.writeHead(403)
+			response.end()
+			return
+		} else {
+			response.writeHead(404, {"Content-Type": mimeTypes[".txt"] })
+			response.end("deletion successful")
+		}
+	}
+
 	if (request.method == "POST") {
 		console.log(request.headers)
+		if (request.headers.date == null) {
+			response.writeHead(418, {"Content-Type": mimeTypes[".txt"]})
+			response.end("You may not brew coffee with a teapot")
+		}
 		request.on("data", chunk => {
 			fs.writeFileSync("forms/" + request.headers.date, chunk)
 		})
-		response.writeHead(404, { "Content-Type": "text/plain" })
-		response.end("recieved")
+		response.writeHead(404, { "Content-Type": mimeTypes[".txt"] })
+		response.end("received")
 		return
 	}
 
@@ -42,7 +61,7 @@ http.createServer(options, (request: http.IncomingMessage, response: http.Server
 	} else if (request.url.startsWith("/templates")) {
 		request.url = "." + request.url
 		if (!fs.existsSync(request.url)) {
-			response.writeHead(404, { "Content-Type": "text/plain" })
+			response.writeHead(404, { "Content-Type": mimeTypes[".txt"] })
 			response.end("Template not found")
 		} else {
 			response.writeHead(200, { "Content-Type": mimeTypes[".json"] })
@@ -51,25 +70,23 @@ http.createServer(options, (request: http.IncomingMessage, response: http.Server
 		return
 	}
 
-	var filePath = './website' + request.url;
+	var filePath = "./website" + request.url;
 	if (filePath.endsWith("/")) {
 		filePath += 'index.html';
 	}
 
 	fs.readFile(filePath, (error, data) => {
 		if (error) {
-			if(error.code == 'ENOENT') {
-				fs.readFile('./404.html', function(error, data) {
-					response.writeHead(404, { 'Content-Type': mimeTypes[".html"] });
-					response.end(data);
-				});
+			if(error.code == "ENOENT") {
+				response.writeHead(301,{ "Location": `http://${request.headers.host}/`});
+				response.end();
 			} else {
-				response.writeHead(404, { "Content-Type": mimeTypes[".html"] })
-				response.end("Unknown Error")
+				response.writeHead(500, { "Content-Type": mimeTypes[".html"] })
+				response.end(`<h1>Error: <code>${error.message}</code></h1>`, "utf-8")
 			}
 		} else {
-			response.writeHead(200, { 'Content-Type': mimeTypes[path.extname(filePath)] || "application/octet-stream" });
-			response.end(data, 'utf-8');
+			response.writeHead(200, { "Content-Type": mimeTypes[path.extname(filePath)] || "application/octet-stream" });
+			response.end(data, "utf-8");
 		}
 	})
 }).listen(80)
@@ -82,6 +99,11 @@ function apiHandler(request: http.IncomingMessage, response: http.ServerResponse
 	} else if (/\/api\/forms\/[\w -.]/g.test(request.url)) {
 		response.writeHead(200, { "Content-Type": mimeTypes[".json"] })
 		response.end(fs.readFileSync("./forms/" + path.parse(request.url).base))
+		return
+	} else if (/\/api\/table\/[\w -.]/g.test(request.url)) {
+		response.writeHead(200, { "Content-Type": mimeTypes[".txt"] })
+		response.end(generateTable(path.parse(request.url).base), "utf-8")
+		return
 	}
 
 	switch (request.url) {
@@ -102,11 +124,14 @@ function apiHandler(request: http.IncomingMessage, response: http.ServerResponse
 		var files = fs.readdirSync("forms")
 		var data = Array<object>()
 		for (var f of files) {
+			if (path.extname(f) != ".json") {
+				continue
+			}
 			var jsonData = JSON.parse(fs.readFileSync("forms/" + f).toString())
 			data.push({
 				"file": f,
 				"template": jsonData["template"],
-				"author": jsonData["author"],
+				"author": jsonData["scout_name"],
 				"size": readableFileSize(fs.statSync("forms/" + f).size)
 			})
 		}
@@ -119,4 +144,40 @@ function readableFileSize(size: number): string {
 	const units = ["B", "KB", "MB", "GB", "TB"]
 	var digitGroup = Math.floor(Math.log(size) / Math.log(1024))
 	return Number((size / Math.pow(1024, digitGroup)).toFixed(2)) + " " + units[digitGroup]
+}
+
+function generateTable(template: string): string {
+	var templateData = JSON.parse(fs.readFileSync("templates/" + template).toString())
+	var tabs = Object.keys(templateData)
+	tabs.splice(tabs.indexOf("title"), 1)
+	var keys = Array<string>()
+	var response = "<thead>"
+	tabs.forEach(value => {
+		var components = templateData[value] as Array<object>
+		components.forEach(value => {
+			if (value["id"] != null) {
+				keys.push(value["id"])
+				response += `<th>${value["id"]}</th>`
+			}
+		})
+	})
+	response += "</thead>"
+	for (var f of fs.readdirSync("forms")) {
+		if (path.extname(f) != ".json") {
+			continue
+		}
+		var formData = JSON.parse(fs.readFileSync("forms/" + f).toString())
+		if (formData["template"] != template) continue
+		response += "<tr>"
+		keys.forEach(value => {
+			if (formData[value] != null) {
+				response += `<td>${formData[value]}</td>`
+			} else {
+				response += "<td>-</td>"
+			}
+		})
+		response += "</tr>"
+	}
+
+	return response
 }
